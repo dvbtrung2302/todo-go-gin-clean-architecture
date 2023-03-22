2b46cd97f3a1d148c2d27c4fa34ed53245df23da
dvbtrung2302 <baotrung23022000@gmail.com>
2023-03-23 01:37:53 +0700

    refactor: add handler

---
+++ handler/todo.go
@@ -0,0 +1,110 @@
+package handler
+
+import (
+	"net/http"
+	"todo-backend/entity"
+
+	"github.com/gin-gonic/gin"
+)
+
+type TodoRepo interface {
+	CreateTodo(todo *entity.Todo) (*entity.Todo, error)
+	GetAllTodos() ([]*entity.Todo, error)
+	GetTodoById(id string) (*entity.Todo, error)
+	UpdateTodoById(id string, todo *entity.Todo) (*entity.Todo, error)
+	DeleteTodoById(id string) (*entity.Todo, error)
+}
+
+type TodoHandler struct {
+	TodoService TodoRepo
+}
+
+func CreateTodoHandler(r *gin.Engine, todoService TodoRepo) TodoHandler {
+	return TodoHandler{todoService}
+}
+
+func (th *TodoHandler) CreateTodo(ctx *gin.Context) {
+	var todo entity.Todo
+	if err := ctx.ShouldBindJSON(&todo); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	newTodo, err := th.TodoService.CreateTodo(&todo)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"data created": newTodo})
+}
+
+func (th *TodoHandler) GetAllTodos(ctx *gin.Context) {
+	todos, err := th.TodoService.GetAllTodos()
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"datas": todos})
+}
+
+func (th *TodoHandler) GetTodoById(ctx *gin.Context) {
+	idStr := ctx.Param("id")
+
+	todo, err := th.TodoService.GetTodoById(idStr)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"data": todo})
+}
+
+func (th *TodoHandler) UpdateTodoById(ctx *gin.Context) {
+	idStr := ctx.Param("id")
+
+	_, err := th.TodoService.GetTodoById(idStr)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	var tempTodo entity.Todo
+	if err := ctx.ShouldBindJSON(&tempTodo); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if tempTodo.Content == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "content cannot be empty"})
+		return
+
+	}
+
+	todo, err := th.TodoService.UpdateTodoById(idStr, &tempTodo)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"data updated": todo})
+}
+
+func (th *TodoHandler) DeleteTodoById(ctx *gin.Context) {
+	idStr := ctx.Param("id")
+
+	_, err := th.TodoService.GetTodoById(idStr)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	todo, err := th.TodoService.DeleteTodoById(idStr)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"data deleted": todo})
+}
--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"net/http"
-	"todo-backend/entity"
+	"todo-backend/handler"
 	"todo-backend/repo"
 	"todo-backend/service"
 
@@ -14,110 +13,15 @@ func main() {
 
 	todoRepo := repo.CreateTodoRepo()
 	todoService := service.CreateTodoService(todoRepo)
+	todoHandler := handler.CreateTodoHandler(router, todoService)
 
 	{
-		router.POST("/items", createTodo(todoService))
-		router.GET("/items", getAllTodos(todoService))
-		router.GET("/items/:id", getTodoById(todoService))
-		router.PUT("/items/:id", updateTodoById(todoService))
-		router.DELETE("/items/:id", deleteTodoById(todoService))
+		router.POST("/items", todoHandler.CreateTodo)
+		router.GET("/items", todoHandler.GetAllTodos)
+		router.GET("/items/:id", todoHandler.GetTodoById)
+		router.PUT("/items/:id", todoHandler.UpdateTodoById)
+		router.DELETE("/items/:id", todoHandler.DeleteTodoById)
 	}
 
 	router.Run()
-}
-
-func createTodo(todoService service.TodoRepo) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		var todo entity.Todo
-		if err := ctx.ShouldBindJSON(&todo); err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		newTodo, err := todoService.CreateTodo(&todo)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		ctx.JSON(http.StatusOK, gin.H{"data created": newTodo})
-	}
-}
-
-func getAllTodos(todoService service.TodoRepo) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		todos, err := todoService.GetAllTodos()
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		ctx.JSON(http.StatusOK, gin.H{"datas": todos})
-	}
-}
-
-func getTodoById(todoService service.TodoRepo) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		idStr := ctx.Param("id")
-
-		todo, err := todoService.GetTodoById(idStr)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		ctx.JSON(http.StatusOK, gin.H{"data": todo})
-	}
-}
-
-func updateTodoById(todoService service.TodoRepo) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		idStr := ctx.Param("id")
-
-		_, err := todoService.GetTodoById(idStr)
-		if err != nil {
-			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
-			return
-		}
-
-		var tempTodo entity.Todo
-		if err := ctx.ShouldBindJSON(&tempTodo); err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		if tempTodo.Content == "" {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "content cannot be empty"})
-			return
-
-		}
-
-		todo, err := todoService.UpdateTodoById(idStr, &tempTodo)
-		if err != nil {
-			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
-			return
-		}
-
-		ctx.JSON(http.StatusOK, gin.H{"data updated": todo})
-	}
-}
-
-func deleteTodoById(todoService service.TodoRepo) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		idStr := ctx.Param("id")
-
-		_, err := todoService.GetTodoById(idStr)
-		if err != nil {
-			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
-			return
-		}
-
-		todo, err := todoService.DeleteTodoById(idStr)
-		if err != nil {
-			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
-			return
-		}
-
-		ctx.JSON(http.StatusOK, gin.H{"data deleted": todo})
-	}
 }
